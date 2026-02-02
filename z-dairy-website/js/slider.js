26269e793b373b894f5e35863f1f4708f414e78c
pankaj <sulakhepankaj76@gmail.com>
2026-02-02 09:28:04 +0530

    updated files

--- z-dairy-website/js/slider.js
+++ z-dairy-website/js/slider.js
@@ -6,3 +6,24 @@ setInterval(() => {
   slides[i].classList.add("active");
   i = (i + 1) % slides.length;
 }, 4000);
+
+// ============== fun-2
+
+  const modal = document.getElementById("viewModal");
+  const modalImg = document.getElementById("modalImg");
+  const modalTitle = document.getElementById("modalTitle");
+  const closeBtn = document.querySelector(".close");
+
+  document.addEventListener("click", e => {
+    const btn = e.target.closest(".action-button");
+    if (!btn) return;
+
+    const card = btn.closest(".card");
+    modalImg.src = card.querySelector("img").src;
+    modalTitle.innerText = card.querySelector("h3").innerText;
+
+    modal.style.display = "flex";
+  });
+
+  closeBtn.onclick = () => modal.style.display = "none";
+
